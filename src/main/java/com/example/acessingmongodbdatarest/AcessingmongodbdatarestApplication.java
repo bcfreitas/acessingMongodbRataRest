6522c0a76105d053c313cbcc4c82e90d274a7199
Bruno Freitas <bcfreitas@gmail.com>
2021-10-21 22:59:23 -0300

    commit inicial

---
+++ src/main/java/com/example/acessingmongodbdatarest/AcessingmongodbdatarestApplication.java
@@ -0,0 +1,13 @@
+package com.example.acessingmongodbdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessingmongodbdatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessingmongodbdatarestApplication.class, args);
+	}
+
+}
